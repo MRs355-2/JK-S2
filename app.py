47428e2fbd935ea5d55acea4c5498ae10a7fd8e9
MRs355-2 <kanao832rsmr@gmail.com>
2021-09-29 02:00:55 +0000

    app.py Risako!

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 app = Flask(__name__)
+db = SQL("sqlite:///texpo.db")
 
 @app.route('/')
 def index():
@@ -8,6 +9,12 @@ def index():
 @app.route('/another')
 def second():
     return render_template('another.html')
-
+    
+@app.route('/upload', method=["GET", "POST"])
+def upload():
+    db.execute = ("INSERT INTO post (title, sport, content) VALUES (?, ?, ?)", 
+                  request.form.get("title"), request.form.get("sport"), request.form.get("content"))
+    return redirect("/")
+    
 app.debug =  True
 app.run()
