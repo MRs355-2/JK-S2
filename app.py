0c53d624b4384102f2a50be41a9944c367b6821a
MRs355-2 <kanao832rsmr@gmail.com>
2021-10-02 04:04:22 +0000

    変更ほぼなし

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
+import sqlite3
 from flask import Flask,render_template
 app = Flask(__name__)
-db = SQL("sqlite:///texpo.db")
+conn = sqlite3.connect('texpo.db') 
+c = conn.cursor() 
+
 
 @app.route('/')
-def index():
+def index(): 
     return render_template('index.html')
 
 @app.route('/another')
 def second():
     return render_template('another.html')
-    
-@app.route('/upload', method=["GET", "POST"])
+
+@app.route('/upload', methods=["GET", "POST"])
 def upload():
-    db.execute = ("INSERT INTO post (title, sport, content) VALUES (?, ?, ?)", 
+    c.execute = ("INSERT INTO post (title, sport, content) VALUES (?, ?, ?)", 
                   request.form.get("title"), request.form.get("sport"), request.form.get("content"))
     return redirect("/")
-    
+
 app.debug =  True
 app.run()
