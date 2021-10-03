b54ba15d81930e232989fd5563f619215352ef49
Basstubass <0130tubasa@gmail.com>
2021-10-03 15:08:19 +0900

    search_System complete

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import sqlite3
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__, static_folder='./templates/images')
-db_conect = sqlite3.connect("texpo.db")
+db_conect = sqlite3.connect("texpo.db", check_same_thread=False)
 cursor = db_conect.cursor()
 
 
@@ -24,9 +24,26 @@ def upload():
 def search():
     return render_template("search.html")
 
-@app.route("/search_pattern", method=["POST"])
-def search_pattern():
     
+@app.route("/search_pattern", methods=["POST"])
+def search_pattern():
+    search_name  = request.form["Sport"]
+    cursor.execute("select sport from post where sport like ?",(search_name,))
+    search_result = cursor.fetchall()
+
+    if search_result ==[]:
+        print("該当なし")
+        return render_template("index.html",sport_name = search_result)
+        #HTML上にアラートとして出力
+    else:
+        return render_template("index.html",sport_name = search_result)
+        print("ui")
+    cursor.close()
+
+db_conect.commit()
+# cursor.close()
+db_conect.commit()
+
 app.debug =  True
 app.run()
 
