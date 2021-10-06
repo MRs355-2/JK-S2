64208ce0c2c7fe9acad4646a3117fa21aa00ed88
MRs355-2 <kanao832rsmr@gmail.com>
2021-10-06 03:23:19 +0000

    rscodeにmasterのクローンを保存

--- app.py
+++ app.py
@@ -9,14 +9,19 @@ c = conn.cursor()
 def index(): 
     return render_template('index.html')
 
-@app.route('/another')
-def second():
+@app.route('/another', methods=["GET", "POST"])
+def second(): 
+    cursor.execute = ("INSERT INTO post (title, sport, content) VALUES (?, ?, ?)",
+                  request.form.get("title"), request.form.get("sport"), request.form.get("content"))
     return render_template('another.html')
 
 @app.route('/upload', methods=["GET", "POST"])
 def upload():
+<<<<<<< Updated upstream
     c.execute = ("INSERT INTO post (title, sport, content) VALUES (?, ?, ?)", 
                   request.form.get("title"), request.form.get("sport"), request.form.get("content"))
+=======
+>>>>>>> Stashed changes
     return redirect("/")
 
 app.debug =  True
