0a8514bc2a01816d4b78e771ebb8ca6a343dc506
MRs355-2 <kanao832rsmr@gmail.com>
2021-10-17 21:06:52 +0000

    いいね機能を追加。

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ cursor = db_connect.cursor()
 #ここで選択できるスポーツを追加
 sports_name = ["サッカー", "テニス", "ゴルフ", "バレー"]
 
+
 @app.route("/")
 def search():
     return render_template("search.html")
@@ -21,13 +22,13 @@ def search():
 def search_pattern():
 
     search_name = request.form["Sport"]
-    cursor.execute("select title, sport, content, id, like from post where sport like ?",(search_name,))
+    cursor.execute("select title, sport, content, id, likes from post where sport like ?",(search_name,))
     search_result = cursor.fetchall()
     if search_result ==[]:
         notfound = "検索結果が見つかりませんでした。"
         return render_template("search.html", notfound=notfound)
     else:
-        return render_template("search.html",search_result=search_result)
+        return render_template("search.html",search_result=search_result, search_name=search_name)
 
     cursor.close()
 
@@ -40,6 +41,26 @@ def upload():
     cursor.execute("INSERT INTO post (title, sport, content) VALUES (?, ?, ?)",
                   [request.form.get("title"), request.form.get("sport"), request.form.get("content")])
 
+    return redirect("/")
+
+@app.route('/like', methods=["GET", "POST"])
+def like():
+    
+    #like数を一つ増やす
+    cursor.execute("select likes from post where id = ?", (request.form.get("like"),))
+    likesplus = cursor.fetchall()
+    likesplus = likesplus[0][0] + 1
+    cursor.execute("UPDATE post SET likes = ? WHERE id = ?", (likesplus, request.form.get("like")))
+    db_connect.commit()
+    
+    #ここで以前検索したページの情報を取りたいんですけど、このsearch_nameをどうやって取得するのかがわかりません
+    #cursor.execute("select title, sport, content, id, likes from post where sport like ?",(search_name,))
+    #search_result = cursor.fetchall()
+    
+    #likedはsearch.htmlでif文を使うために設定しました。とくに関係ありません。post_idはそのidの投稿に飛べるようにと思ったんですが、まだhtmlではなにもしていません。
+    #return render_template("search.html",search_result=search_result, liked=True, post_id=request.form.get("like"))
+    
+    #とりあえず最初の検索画面に飛べるようにします
     return redirect("/")
 
 db_connect.commit()
