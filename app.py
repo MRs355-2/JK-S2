6091441b0b9788022c4f21f351644edf3774f482
Basstubass <0130tubasa@gmail.com>
2021-10-18 13:54:56 +0900

    hello

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from contextlib import closing
-
+import socket
 
 app = Flask(__name__, static_folder='./static')
 
@@ -12,22 +12,53 @@ cursor = db_connect.cursor()
 #ここで選択できるスポーツを追加
 sports_name = ["サッカー", "テニス", "ゴルフ", "バレー"]
 
+user_host = socket.gethostname()
+user_ip = socket.gethostbyname(user_host)
+
 @app.route("/")
 def search():
+    print(user_ip)
     return render_template("search.html")
 
     
-@app.route("/search_pattern", methods=["POST"])
+@app.route("/search_pattern", methods=["GET","POST"])
 def search_pattern():
-
     search_name = request.form["Sport"]
-    cursor.execute("select title, sport, content, id from post where sport like ?",(search_name,))
-    search_result = cursor.fetchall()
-    if search_result ==[]:
-        notfound = "検索結果が見つかりませんでした。"
-        return render_template("search.html", notfound=notfound)
+    sort_result = request.form.get("sort")
+
+    if search_name == "":
+        return render_template("search.html")
+
     else:
-        return render_template("search.html",search_result=search_result)
+        if sort_result == "id":
+            cursor.execute("select title, sport, content, id from post where sport like ? order by id desc;",(search_name,))
+            search_result = cursor.fetchall()
+        elif sort_result == "like":
+            cursor.execute("select title, sport, content, id from post where sport like ? order by like desc;",(search_name,))
+            search_result = cursor.fetchall()
+        else:
+            cursor.execute("select title, sport, content, id from post where sport like ?",(search_name,))
+            search_result = cursor.fetchall()
+
+    if search_result ==[]:
+
+        if sort_result == "id":
+            cursor.execute("select title, sport, content, id from post where title like ? order by id desc;",("%"+ search_name +"%",))
+            search_result = cursor.fetchall()
+        elif sort_result == "like":
+            cursor.execute("select title, sport, content, id from post where title like ? order by like desc;",("%"+ search_name +"%",))
+            search_result = cursor.fetchall()
+        else:
+            cursor.execute("select title, sport, content, id from post where title like ?",("%"+ search_name +"%",))
+            search_result = cursor.fetchall()
+
+        if search_result ==[]:
+            notfound = "検索結果が見つかりませんでした。"
+            return render_template("search.html", notfound=notfound)
+        else:
+            return render_template("search.html",search_result=search_result)
+    else:
+            return render_template("search.html",search_result=search_result)
 
     cursor.close()
 
@@ -47,6 +78,10 @@ db_connect.commit()
 # cursor.close()
 
 
+#####テスト#####
+
+
+#####テスト#####
 
 app.debug =  True
 app.run()
